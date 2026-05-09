d208fe08dd43ef78141c6e2c08145b9c95731615
DannyRidz <danny.ridz93@gmail.com>
2026-05-09 20:15:55 +0800

    Complete 08_removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (firstArg, ...theRest) {
+    return firstArg.filter(element => !theRest.includes(element))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
