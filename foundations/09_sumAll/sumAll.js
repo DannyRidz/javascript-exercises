51233e6ae275c3aa61a1b6cb73402e2fc8366923
DannyRidz <danny.ridz93@gmail.com>
2026-05-09 20:54:40 +0800

    Complete 09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let total = 0;
 
+    if (typeof a !== "number" || a < 0 || !Number.isInteger(a) ||
+        typeof b !== "number" || b < 0 || !Number.isInteger(b)) {
+
+        return "ERROR"
+    }
+
+    for (i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
