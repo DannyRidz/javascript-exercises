68085048017ca21d476df2b200b5f5c1a41d9257
DannyRidz <danny.ridz93@gmail.com>
2026-05-15 13:57:11 +0800

    Complete 13_palindromes exercise.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (strings) {
+    let string = strings.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversedString = string.split('').reverse().join('');
+    return string === reversedString;
 };
+
+console.log(palindromes("race car"))
 
 // Do not edit below this line
 module.exports = palindromes;
