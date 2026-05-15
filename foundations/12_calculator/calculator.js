1ad8dd2ac82c1f0d0750228e523512303b5daeeb
DannyRidz <danny.ridz93@gmail.com>
2026-05-15 11:02:32 +0800

    Complete 12_calculator exercise.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let initialValue = 1;
+  for (let i = number; i > 0; i--) {
+    initialValue *= i;
+  }
+
+  return initialValue;
 };
 
 // Do not edit below this line
