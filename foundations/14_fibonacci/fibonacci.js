2752d9cb74b5dc3a9518006625989d00cd150021
DannyRidz <danny.ridz93@gmail.com>
2026-05-15 14:28:45 +0800

    Complete 14_fibonacci exercise.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+    input = Number(input);
 
+    if (input < 0) {
+        return "OOPS";
+    }
+    else if (input === 0) {
+        return 0;
+    }
+
+    let a = 0;
+    let b = 1;
+    let c;
+
+    for (let i = 2; i <= input; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
