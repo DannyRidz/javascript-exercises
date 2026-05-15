742072d39e29ca8048cda00ca779d00fb7baaee0
DannyRidz <danny.ridz93@gmail.com>
2026-05-15 16:23:30 +0800

    Complete 16_findTheOldest exercise.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const currentYear = new Date().getFullYear();
 
+    return people.reduce((oldest, person) => {
+        const oldestEndYear = oldest.yearOfDeath || currentYear;
+        const personEndYear = person.yearOfDeath || currentYear;
+
+        const oldestAge = oldestEndYear - oldest.yearOfBirth;
+        const personAge = personEndYear - person.yearOfBirth;
+
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
